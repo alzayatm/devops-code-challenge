62e8ded9d3e0fdf8001c908bbe55d31cf34169c0
Mihad Alzayat <alzayatm@gmail.com>
2025-04-29 19:01:22 -0400

    .

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -37,3 +37,8 @@ output "backend_alb_dns" {
   value = aws_lb.backend_alb.dns_name
   description = "Public URL of the backend API"
 }
+
+output "frontend_alb_dns" {
+  value = aws_lb.frontend_alb.dns_name
+  description = "Public URL of the frontend app"
+}
